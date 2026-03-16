5015fabac881fe48cf3c0f014d098f2d10b4562f
Joseph Simpson <joe@radicalappdev.com>
2026-03-16 18:21:33 -0400

    Toggle debug button in the space when setting changes

--- SharedVisions/SharedVisions/Views/ImmersiveView.swift
+++ SharedVisions/SharedVisions/Views/ImmersiveView.swift
@@ -12,21 +12,30 @@ import RealityKitContent
 struct ImmersiveView: View {
     @Environment(AppModel.self) private var appModel
 
+    @State var debug = Entity()
+
     var body: some View {
         RealityView { content in
             // Add the initial RealityKit content
             guard let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) else { return }
             content.add(scene)
 
-            if(appModel.useDebugMode) {
-                let debug = Entity()
-                debug.position = SIMD3<Float>(0.75, 0.25, -1.25)
-                let attachment = ViewAttachmentComponent(rootView: ToggleDebugWindowButton())
-                debug.components.set(attachment)
-                debug.components.set(BillboardComponent())
-                scene.addChild(debug)
-            }
+            debug.position = SIMD3<Float>(0.75, 0.25, -1.25)
+            let attachment = ViewAttachmentComponent(rootView: ToggleDebugWindowButton())
+            debug.components.set(attachment)
+            debug.components.set(BillboardComponent())
+            scene.addChild(debug)
 
+
+        }
+        .onChange(of: appModel.useDebugMode) { _, newValue in
+            if(newValue) {
+                print("Debug mode is on")
+                debug.isEnabled = true
+            } else {
+                print("Debug mode is off")
+                debug.isEnabled = false
+            }
         }
     }
 }
