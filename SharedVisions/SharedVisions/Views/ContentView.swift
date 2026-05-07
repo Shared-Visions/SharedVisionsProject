170a9f557f0d128dddc8666b821a321914828bb4
Joseph Simpson <joe@radicalappdev.com>
2026-05-07 05:37:10 -0400

    CYA

--- SharedVisions/SharedVisions/Views/ContentView.swift
+++ SharedVisions/SharedVisions/Views/ContentView.swift
@@ -15,10 +15,9 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
 
             Text("Shared Visions")
+                .font(.largeTitle)
 
             ToggleImmersiveSpaceButton()
         }
